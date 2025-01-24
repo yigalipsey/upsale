9cb7f54494dbce24504560130ca59fe65738d82c
yigalipsey <yigalipsey1996@example.com>
2025-01-24 15:42:05 +0700

    ok

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import AboutSales from "./components/AboutSales";
 import AboutOutsourcing from "./components/AboutOutsourcing";
 import WhyChooseUs from "./components/WhyChooseUs";
 import AboutUs from "./components/AboutUs";
